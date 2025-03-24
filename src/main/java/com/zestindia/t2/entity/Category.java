1cb2d158847bd276679e6a61e8813150fba1777a
shubhamk38 <jadoo.8k@gmail.com>
2025-03-24 18:40:58 +0530

    master entity update

--- src/main/java/com/zestindia/t2/entity/Category.java
+++ src/main/java/com/zestindia/t2/entity/Category.java
@@ -2,10 +2,8 @@ package com.zestindia.t2.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-
 
 
 @Entity
@@ -15,7 +13,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Category extends  MasterEntity {
+public class Category extends MasterEntity {
 
     @NotNull(message = "Category Name Should Not Null")
     @Column(unique = true)
